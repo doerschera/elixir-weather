8afc92e7ef0275b728e54cc7843809c0f6e984b0
doerschera <doerschera@gmail.com>
2017-11-19 19:18:14 -0800

    print weather

--- weather/lib/weather/cli.ex
+++ weather/lib/weather/cli.ex
@@ -1,6 +1,34 @@
 defmodule Weather.CLI do
-  def main(args) do
+  def main([args | _]) do
     args
+    |> create_url
+    |> request_weather
+    |> parse_weather
     |> IO.inspect
+  end
+
+  def create_url(args) do
+    # TODO: hide key
+    key = "f05db94200ce821e129cc86d109f222c"
+    "api.openweathermap.org/data/2.5/weather?zip=#{args},us&units=imperial&appid=#{key}"
+  end
+
+  def request_weather(url) do
+    HTTPoison.get(url)
+  end
+
+  def parse_weather({:ok, %{body: response}}) do
+    case Poison.Parser.parse(response) do
+      {:ok, %{"cod" => "404", "message" => message}} -> message
+      {:ok, body} -> format_weather(body)
+    end
+  end
+
+  def format_weather(%{
+    "main" => %{"temp" => temp},
+    "name" => name,
+    "weather" => [%{"description" => description}]
+  }) do
+    "It is currently #{temp} is #{name} with #{description}."
   end
 end
