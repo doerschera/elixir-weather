e8fe05c14f5d4f8d896e3adbcba537e5fd5b1b89
doerschera <doerschera@gmail.com>
2017-11-19 19:44:01 -0800

    privatize functions

--- weather/lib/weather/cli.ex
+++ weather/lib/weather/cli.ex
@@ -5,26 +5,26 @@ defmodule Weather.CLI do
     |> IO.inspect
   end
 
-  def process_args([zip | _]) do
+  defp process_args([zip | _]) do
     create_url(zip)
   end
 
-  def process_args([]) do
+  defp process_args([]) do
     "Weather balloon error: please enter a zip code."
   end
 
-  def create_url(zip) do
+  defp create_url(zip) do
     # TODO: hide key
     key = "f05db94200ce821e129cc86d109f222c"
     url = "api.openweathermap.org/data/2.5/weather?zip=#{zip},us&units=imperial&appid=#{key}"
     request_weather(url)
   end
 
-  def request_weather(url) do
+  defp request_weather(url) do
     parse_weather(HTTPoison.get(url))
   end
 
-  def parse_weather({:ok, %{body: response}}) do
+  defp parse_weather({:ok, %{body: response}}) do
     case Poison.Parser.parse(response) do
       {:ok, %{"cod" => "404", "message" => message}} -> "Weather balloon error: #{message}."
       {:ok, body} -> format_weather(body)
