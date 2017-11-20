25ea5ea6565c61b6bc94a36b9a2bc72ebddd4937
doerschera <doerschera@gmail.com>
2017-11-19 19:23:23 -0800

    handle empty args

--- weather/lib/weather/cli.ex
+++ weather/lib/weather/cli.ex
@@ -1,25 +1,32 @@
 defmodule Weather.CLI do
-  def main([args | _]) do
+  def main(args) do
     args
-    |> create_url
-    |> request_weather
-    |> parse_weather
+    |> process_args
     |> IO.inspect
   end
 
-  def create_url(args) do
+  def process_args([zip | _]) do
+    create_url(zip)
+  end
+
+  def process_args([]) do
+    "Weather balloon error: please enter a zip code."
+  end
+
+  def create_url(zip) do
     # TODO: hide key
     key = "f05db94200ce821e129cc86d109f222c"
-    "api.openweathermap.org/data/2.5/weather?zip=#{args},us&units=imperial&appid=#{key}"
+    url = "api.openweathermap.org/data/2.5/weather?zip=#{zip},us&units=imperial&appid=#{key}"
+    request_weather(url)
   end
 
   def request_weather(url) do
-    HTTPoison.get(url)
+    parse_weather(HTTPoison.get(url))
   end
 
   def parse_weather({:ok, %{body: response}}) do
     case Poison.Parser.parse(response) do
-      {:ok, %{"cod" => "404", "message" => message}} -> message
+      {:ok, %{"cod" => "404", "message" => message}} -> "Weather balloon error: #{message}."
       {:ok, body} -> format_weather(body)
     end
   end
